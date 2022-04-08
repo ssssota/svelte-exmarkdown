43aeb89601fd2025a227d63dffe8c4a0e3b3b62d
ssssota <tommysouta@gmail.com>
2022-04-08 23:45:58 +0900

    lint/fmt

--- src/lib/renderer/Code.svelte
+++ src/lib/renderer/Code.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let lang: string | null = null;
 	export let meta: string | null = null;
-	export let value: string = '';
+	export let value = '';
 </script>
 
 <pre><code class={lang !== null ? `language-${lang}` : ''}>{value.trim()}</code></pre>
--- src/lib/renderer/Image.svelte
+++ src/lib/renderer/Image.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let title: string | null = null;
 	export let url: string;
-	export let alt: string = '';
+	export let alt = '';
 </script>
 
 <img src={url} {alt} {title} />
