2c4dc9f1e7361f1711984f858bc5243b3b1263ed
ssssota <tommysouta@gmail.com>
2022-04-09 10:35:06 +0900

    organize deps

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,9 @@
 import type { SvelteComponent } from 'svelte';
-import type { Pluggable } from 'unified';
-export type { Node } from 'unist';
+import type { FrozenProcessor, Pluggable } from 'unified';
 
 export type ComponentsMap = Record<string, typeof SvelteComponent>;
 export type Plugin = {
 	remarkPlugin?: Pluggable;
 	renderer?: ComponentsMap;
 };
+export type Node = ReturnType<FrozenProcessor['parse']>;
