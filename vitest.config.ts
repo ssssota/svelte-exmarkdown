4f7f8dcdf095cfa8c8dbec7b4bdb2f77fed7a454
ssssota <tommysouta@gmail.com>
2022-04-10 17:20:05 +0900

    ci: setup test

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	plugins: [svelte({ hot: !process.env.VITEST }), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: 'jsdom'
+	}
+});
