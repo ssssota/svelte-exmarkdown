ebece21a9786922852e66906410256684c2821a2
ssssota <ssssota@users.noreply.github.com>
2023-10-30 01:36:23 +0000

    deploy: c37571a37411e77ee8268bdee18c06795967e886

---
+++ _app/immutable/chunks/stores.1ed82c43.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.2a4e3d2b.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
