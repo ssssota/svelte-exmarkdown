d1cea5546841cd5f3a7fac5297984583ddad8dc0
Jaco Kok <kokjaco2@gmail.com>
2024-01-10 11:38:29 +0200

    Created new Element and ElementSVG components to be used in place of svelte:element. This will set namespace and fix svg render issue.

---
+++ src/lib/Element/Element.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ElementSvg from './ElementSVG.svelte';
+	export let component: string;
+</script>
+
+{#if component === 'svg' || component === 'path'}
+	<ElementSvg {component} {...$$restProps}><slot /></ElementSvg>
+{:else}
+	<svelte:element this={component} {...$$restProps}>
+		<slot />
+	</svelte:element>
+{/if}
--- src/lib/Renderer.svelte
+++ src/lib/Renderer.svelte
@@ -7,6 +7,7 @@
 	import type { HastNode } from './types';
 	import { resolveComponent } from './utils';
 	export let astNode: HastNode;
+	import Element from './Element/Element.svelte';
 
 	const components = getComponentsMap();
 
@@ -23,14 +24,14 @@
 		resolveComponent(
 			$components,
 			astNode.tagName
-		)}{#if typeof component === 'string'}{#if Array.isArray(astNode.children) && astNode.children.length !== 0}<svelte:element
-				this={component}
+		)}{#if typeof component === 'string'}{#if Array.isArray(astNode.children) && astNode.children.length !== 0}<Element
+				component={component}
 				{...astNode.properties}
 				>{#each astNode.children as child}<svelte:self
 						astNode={child}
-					/>{/each}</svelte:element
-			>{:else}<svelte:element
-				this={component}
+					/>{/each}</Element
+			>{:else}<Element
+				component={component}
 				{...astNode.properties}
 			/>{/if}{:else if component !== null}{#if Array.isArray(astNode.children) && astNode.children.length !== 0}<svelte:component
 				this={component}
