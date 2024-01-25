0ee4065f370daf141648f7ed4342c7594969cab0
TOMIKAWA Sotaro <sotaro.tomikawa@zozo.com>
2024-01-26 02:04:43 +0900

    chore: fix type-check errors

--- src/routes/docs/[slug]/+page.ts
+++ src/routes/docs/[slug]/+page.ts
@@ -5,7 +5,7 @@ export const load = (async ({ parent, params }) => {
 	const data = await parent();
 	const current = data.docs.findIndex((doc) => doc.slug === params.slug);
 	const markdown = data.docs[current]?.markdown;
-	if (!markdown) throw error(404);
+	if (!markdown) error(404);
 	return {
 		markdown,
 		prev: data.docs[current - 1],
