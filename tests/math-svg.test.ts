3305672b55c2073bebeb928b5567aba3497beca4
ssssota <tommysouta@gmail.com>
2024-10-26 11:59:17 +0900

    test: check namespaceURI

---
+++ tests/math-svg.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test';
+import { basePath } from './utils';
+
+test('render images with each plugins', async ({ page }) => {
+	await page.goto(`${basePath}/math-svg`);
+	const textarea = page.getByRole('textbox');
+
+	const getPathNamespace = () => {
+		return page.evaluate(() => {
+			return window.document.querySelector('path')?.namespaceURI;
+		});
+	};
+	expect(getPathNamespace()).resolves.toBe('http://www.w3.org/2000/svg');
+
+	await textarea.clear();
+	await textarea.fill('$$\\sqrt{2}$$');
+	expect(getPathNamespace()).resolves.toBe('http://www.w3.org/2000/svg');
+});
